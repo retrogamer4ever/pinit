369a91a19d234bd99bb03409ef01a11cc911d770
Joseph Burchett <retrogamer4ever@gmail.com>
2019-12-27 17:00:03 -0800

    Updated project name, added more comments and few more fixes

--- mobile/app/src/main/java/com/joecompany/pinit/constants/Facebook.java
+++ mobile/app/src/main/java/com/joecompany/pinit/constants/Facebook.java
@@ -1,9 +1,12 @@
 package com.joecompany.pinit.constants;
 
 public class Facebook {
+
+    // Facebook app doesn't allow you use an intent for the oauth redirect url, only a secure url, so setup a temp callback from personal website
+    public static final String REDIRECT_URI = "https://myremindoapptesting.me/fblogin.html";
+
     public static final String CLIENT_ID = "2566190067002357";
     public static final String CLIENT_SECRET = "dd006b67404848ab9b24c461eed140e3";
-    public static final String REDIRECT_URI = "https://myremindoapptesting.me/fblogin.html";
     public static final String RESPONSE_TYPE = "code";
     public static final String AUTH_TYPE = "reauthenticate";
     public static final String ACCESS_TOKEN = "access_token";
